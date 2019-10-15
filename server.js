e164cc0226dff70458c9b7c012481991181f3f31
Blake Neely <blakeneely21@gmail.com>
2019-10-14 21:20:46 -0500

    add git ignore and procfile

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+// var PORT = 3000;
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -89,6 +94,6 @@ app.post("/api/characters", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
 });
